596778d433102dd3e3187ac65e3a0e8d63d599db
conradoRC <conrado27cym@gmail.com>
2020-06-15 15:55:45 -0500

    FIX BUGgit add .git add .

--- Unidad III/Paint/src/paint/Draw.java
+++ Unidad III/Paint/src/paint/Draw.java
@@ -2,7 +2,6 @@ package paint;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -12,7 +11,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Line2D;
-import java.awt.geom.Line2D.Float;
 import java.util.LinkedList;
 
 import javax.swing.JComponent;
@@ -23,19 +21,20 @@ public class Draw extends JComponent {
 	private Point destino;
 	private int xinit;
 	private int yinit;
-	private IFigures drag = null;
-	private boolean flag = false;
+	private static IFigures drag = null;
+	private boolean flag = true;
 	private LinkedList<IFigures> lineas1 = new LinkedList<IFigures>();
 
 	public Draw() {
 		Controller control = new Controller();
 		addMouseListener(control);
 		addMouseMotionListener(control);
-		test();
+		//test();
 	}
 
 	public void addFigures(IFigures figura) {
 		lineas1.add(figura);
+		repaint();
 	}
 
 	public void paintComponent(Graphics g) {
@@ -47,83 +46,80 @@ public class Draw extends JComponent {
 
 		g2.setColor(Color.BLUE);
 
-		for (IFigures figure : lineas1) {
-			figure.paint_figure(g2);
+		if (!lineas1.isEmpty()) {
+			for (IFigures figure : lineas1)
+				figure.paint_figure(g2);
 		}
-		
-		
+
 		if (origen != null && destino != null && !flag) {
-			Shape linea = new Line2D.Float(origen.x, origen.y, destino.x, destino.y);
-			g2.draw(linea);
+			Shape s =  new Line2D.Float(origen.x, origen.y, destino.x, destino.y);
+			g2.draw(s);
 		}
+		
 	}
 
-	public void test() {
-		IFigures linea = new DrawLine(0, 0, 0, 0);
-		addFigures(linea);
-	}
-	
 	public IFigures checkFigure(MouseEvent e) {
+		System.out.println(lineas1.size());
 		for (IFigures iFigures : lineas1) {
 			if (iFigures.fInside(e.getX(), e.getY())) {
-				System.out.println("check");
 				return iFigures;
 			}
 		}
 		return null;
 	}
+	
+	public void test() {
+		IFigures d = new DrawLine(50, 50, 400, 300);
+		addFigures(d);
+	}
+
+	public void mouseDF(MouseEvent e) {
+		destino = new Point(e.getX(), e.getY());
+		repaint();
+		drag=null;
+	}
+
+	public void mouseDT(MouseEvent e) {
+		try {
+			if (drag == null) {
+				xinit = e.getX();
+				yinit = e.getY();
+				drag = checkFigure(e);
+			} else {
+				drag.setPosicion(drag.getX() + (e.getX() - xinit), drag.getY() + (e.getY() - yinit));
+				xinit = e.getX();
+				yinit = e.getY();
+				repaint();
+			}
+		} catch (Exception ex) {
+		}
+	}
 
 	class Controller implements MouseMotionListener, MouseListener {
 		@Override
 		public void mouseDragged(MouseEvent e) {
-			if(!flag) {
-				drag=null;
-				destino = new Point(e.getX(), e.getY());
-				repaint();
-			}
-			
-			if(flag) {
-				try {
-					System.out.println(drag);
-					if (drag == null) {
-						xinit = e.getX();
-						yinit = e.getY();
-						drag = checkFigure(e);
-					}
-					else {
-						drag.setPosicion(drag.getX() + (e.getX() - xinit),drag.getY() + (e.getY() - yinit));
-						xinit = e.getX();
-						yinit = e.getY();
-						System.out.println(drag);
-						repaint();
-					}	
-				}catch (Exception ex) {
-					System.out.println("error");
-				}
+
+			if (flag) {
+				mouseDT(e);
+			} else {
+				mouseDF(e);
 			}
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent e) {
-			if(flag==true) {
+			if (flag)
 				drag = null;
-			}
-			
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			if (!flag) {
-				origen = new Point(e.getX(), e.getY());
-			}
+			if (!flag) origen = new Point(e.getX(), e.getY());
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			if (!flag) {
-				IFigures linea = new DrawLine(origen.x, origen.y, e.getX(), e.getY());
-				addFigures(linea);
-			}
+			if (!flag) addFigures(new DrawLine(origen.x, origen.y, destino.x, destino.y));
 		}
 
 		@Override
@@ -141,16 +137,13 @@ public class Draw extends JComponent {
 
 	public void reset() {
 		lineas1.clear();
-		
 		repaint();
-		
 	}
 
 	public boolean isFlag() {
 		return flag;
 	}
-	
-	
+
 	public IFigures getDrag() {
 		return drag;
 	}
